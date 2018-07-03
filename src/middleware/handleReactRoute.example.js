0e867296ae880b307fd8384ea192ee116175ba53
Elyse Gordon <egordon@strava.com>
2018-07-03 07:22:00 +0100

    update example file for this branch

--- src/middleware/handleReactRoute.example.js
+++ src/middleware/handleReactRoute.example.js
@@ -3,11 +3,30 @@ import React from 'react';
 import { renderToString } from 'react-dom/server';
 import App from "../App";
 import Index from "../components/Index/Index";
+import initRedux from "../shared/init-redux";
+import { Provider } from 'react-redux';
+import Products from "../components/Products/Products";
 
+export default (req, res) => {
 
-export default handleReactRoute (req, res) => {
-    const html = renderToString(<Provider store={store}>
-        <App />
-    </Provider>);
-    res.send(renderToString(<Index html={html} state={stringifiedServerState}/>));
+    // add redux
+    const store = initRedux();
+    // console.log("STORE---", store)
+    //prefetch some data
+
+    const promises = Products.prefetchActions().map((initialAction) => {
+        return store.dispatch(initialAction());
+    });
+
+    Promise.all(promises).then(() => {
+        const serverState = store.getState();
+        const stringifiedServerState = JSON.stringify(serverState);
+
+        const html = renderToString(<Provider store={store}>
+            <App />
+        </Provider>);
+        res.send(renderToString(<Index html={html} state={stringifiedServerState}/>));
+    }).catch(e => {
+        console.log(e)
+    });
 }
