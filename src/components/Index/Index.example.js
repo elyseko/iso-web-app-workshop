654e46f1da63ec540aa70799a0385a802d4f852b
Elyse Gordon <egordon@strava.com>
2018-07-03 07:25:08 +0100

    Cleanup Index.js and create example code

---
+++ src/components/Index/Index.example.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import config from '../../config';
+
+const Index = (props) => {
+    return (
+        <html>
+        <head>
+            <title>All Things Westies</title>
+            <link rel="stylesheet" type="text/css" href="semantic-ui-css/semantic.css" />
+        </head>
+        <body>
+        <div
+            id="react-content"
+            dangerouslySetInnerHTML={{ __html: props.html }}
+        />
+        <script dangerouslySetInnerHTML={{
+            __html: `
+                    window.__SERIALIZED_STATE__ =
+                      JSON.stringify(${props.state})
+                  `
+        }}
+        />
+        <script src={`${config.webpackServer}/static/js/bundle.js`}></script>
+        </body>
+        </html>
+    )
+}
+
+export default Index;
