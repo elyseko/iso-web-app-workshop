b90611a9ae6eadaa55243798be25b8b8150c0392
Elyse Gordon <egordon@strava.com>
2018-07-03 07:27:40 +0100

    cleanup and reset browser entry point + create example file

--- src/browser-entry-point.js
+++ src/browser-entry-point.js
@@ -6,17 +6,4 @@ import initRedux from './shared/init-redux.js';
 import sharedRoutes from './shared/sharedRoutes';
 import App from "./App";
 
-const initialState = JSON.parse(window.__SERIALIZED_STATE__);
-// console.log(initialState);
-
-const store = initRedux(initialState);
-
-function init() {
-    ReactDOM.hydrate(
-        <Provider store={store}>
-            <App/>
-        </Provider>, document.getElementById('react-content')
-    );
-}
-
-init();
+console.log('The browser code got loaded!!!')
