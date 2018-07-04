25826842b40fa37a68ac530d3436618afc48794c
Elyse Gordon <egordon@strava.com>
2018-07-04 01:51:39 +0100

    reset code to base state for branch

--- src/components/Index/Index.example.js
+++ src/components/Index/Index.example.js
@@ -6,21 +6,21 @@ const Index = (props) => {
         <html>
         <head>
             <title>All Things Westies</title>
-            <link rel="stylesheet" type="text/css" href="semantic-ui-css/semantic.css" />
+            <link rel="stylesheet" type="text/css" href="/assets/semantic-ui-css/semantic.css" />
         </head>
         <body>
-        <div
-            id="react-content"
-            dangerouslySetInnerHTML={{ __html: props.html }}
-        />
-        <script dangerouslySetInnerHTML={{
-            __html: `
-                    window.__SERIALIZED_STATE__ =
-                      JSON.stringify(${props.state})
-                  `
-        }}
-        />
-        <script src={`${config.webpackServer}/static/js/bundle.js`}></script>
+            <div
+                id="react-content"
+                dangerouslySetInnerHTML={{ __html: props.html }}
+            />
+            <script dangerouslySetInnerHTML={{
+                __html: `
+                        window.__SERIALIZED_STATE__ =
+                          JSON.stringify(${props.state})
+                      `
+            }}
+            />
+            <script src={`${config.webpackServer}/static/js/bundle.js`}></script>
         </body>
         </html>
     )
--- src/middleware/handleReactRoute.js
+++ src/middleware/handleReactRoute.js
@@ -7,26 +7,4 @@ import initRedux from "../shared/init-redux";
 import { Provider } from 'react-redux';
 import Products from "../components/Products/Products";
 
-export default (req, res) => {
-
-    // add redux
-    const store = initRedux();
-    // console.log("STORE---", store)
-    //prefetch some data
-
-    const promises = Products.prefetchActions().map((initialAction) => {
-        return store.dispatch(initialAction());
-    });
-
-    Promise.all(promises).then(() => {
-        const serverState = store.getState();
-        const stringifiedServerState = JSON.stringify(serverState);
-
-        const html = renderToString(<Provider store={store}>
-            <App />
-        </Provider>);
-        res.send(renderToString(<Index html={html} state={stringifiedServerState}/>));
-    }).catch(e => {
-        console.log(e)
-    });
-}
+export default (req, res) => {}
